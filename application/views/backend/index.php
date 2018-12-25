6be76f69e84880d2c78f5beb6e34c891fc7723a6
Candra06 <candrabiyu4@gmail.com>
2018-12-25 11:01:19 +0700

    Setting template

--- application/views/backend/index.php
+++ application/views/backend/index.php
@@ -67,10 +67,34 @@
       <?php
         include str_replace("system", "application/views/backend/", BASEPATH)."/layout/head.php";
         include str_replace("system", "application/views/backend/", BASEPATH)."/layout/sidebar.php";
+        
+        
+      ?>
+     <div class="br-mainpanel">
+      <div class="br-pagetitle">
+        <i class="icon ion-ios-home-outline tx-70 lh-0"></i>
+        <div>
+          <h4>Dashboard</h4>
+          <p class="mg-b-0">Do bigger things with Bracket plus, the responsive bootstrap 4 admin template.</p>
+        </div>
+      </div><!-- d-flex -->
+
+      <?php
         include str_replace("system", "application/views/backend/", BASEPATH)."/layout/content.php";
       ?>
 
-
-
+      <footer class="br-footer">
+        <div class="footer-left">
+          <div class="mg-b-2">Copyright &copy; 2017. Bracket Plus. All Rights Reserved.</div>
+          <div>Attentively and carefully made by ThemePixels.</div>
+        </div>
+        <div class="footer-right d-flex align-items-center">
+          <span class="tx-uppercase mg-r-10">Share:</span>
+          <a target="_blank" class="pd-x-5" href="https://www.facebook.com/sharer/sharer.php?u=http%3A//themepixels.me/bracketplus/intro"><i class="fab fa-facebook tx-20"></i></a>
+          <a target="_blank" class="pd-x-5" href="https://twitter.com/home?status=Bracket%20Plus,%20your%20best%20choice%20for%20premium%20quality%20admin%20template%20from%20Bootstrap.%20Get%20it%20now%20at%20http%3A//themepixels.me/bracketplus/intro"><i class="fab fa-twitter tx-20"></i></a>
+        </div>
+      </footer>
+    </div><!-- br-mainpanel -->
 </body>
+
 </html>
